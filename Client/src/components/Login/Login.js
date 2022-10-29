0f023a1abc07477e753bff93ccf258cdbd648152
FinoanaRandria <niriantsoa.randriambololomanana@esti.mg>
2022-10-29 13:28:40 +0300

    inplement

---
+++ Client/src/components/Login/Login.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../Assets/CSS/Login.css"
+export default function Login() {
+  return (
+    <div className='card'>
+        <div className='group'></div>
+
+    </div>
+  )
+}
