1c83ed0db2386044e0c6c59ea6952c7495cca91f
FinoanaRandria <niriantsoa.randriambololomanana@esti.mg>
2022-10-29 15:26:45 +0300

    Seaio

--- Client/src/components/Login/Login.js
+++ Client/src/components/Login/Login.js
@@ -3,20 +3,18 @@ import "../Assets/CSS/Login.css";
 export default function Login() {
   return (
     <div className="card">
-       <div>
-          <input type="text"/>
-       </div>
-          <br/>
-
-          <div>
-          <input type="text"/>
-       </div>
-         <div>
-         <br/>
-         <br/>
-         <button> Button</button>              
-         </div>
-    
+       <h1>Bienvenu Sur Sea </h1>
+      <div>
+        <input type="text" placeholder="Email" />
+      </div>
+      <br />
+      <br />
+      <div>
+        <input type="text" placeholder="Password" />
+      </div>
+      <div>
+        <button>Login</button>
+      </div>
     </div>
   );
 }
