1ebee6c46a1ae47181a0f1b12bad08176493fa38
FinoanaRandria <niriantsoa.randriambololomanana@esti.mg>
2022-10-29 20:18:39 +0300

    test io

--- Client/src/components/Login/Login.js
+++ Client/src/components/Login/Login.js
@@ -1,22 +1,36 @@
-import React from "react";
+import React,{useEffect,useRef} from "react";
 import "../Assets/CSS/Login.css";
+import Lottie from 'lottie-web'
 export default function Login() {
+   const container =useRef(null)
+   useEffect(()=>{
+      Lottie.loadAnimation({
+        container:container.current,
+            renderer:'svg',
+            loop:true,
+        animationData:require('../Assets/svg/25059-gray-seagulls(1).json')
+      })
+   }, [])
+
   return (
     <div className="card">
+    <div className="bir" ref={container}></div>
        <h1>Bienvenu Sur Sea </h1>
       <div>
-        <input type="text" placeholder="Email" />
+        <input type="email" placeholder="Email" />
       </div>
       <br />
       <br />
       <div>
-        <input type="text" placeholder="Password" />
+        <input type="password" placeholder="Password" />
       </div>
       <div>
         <button>Login</button>
       </div>
 
        <p>Design By Finoana Randria</p>
+         
     </div>
+
   );
 }
